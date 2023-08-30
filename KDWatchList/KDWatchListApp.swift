63f6d3a4a73ffc0e603426b5b2d5ec0fe758a1b6
Rob Ranf <rob@emiyaconsulting.com>
2023-08-30 11:59:59 -0700

    Initial Commit

---
+++ KDWatchList/KDWatchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  KDWatchListApp.swift
+//  KDWatchList
+//
+//  Created by Rob Ranf on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct KDWatchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KDWatchListUITests/KDWatchListUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KDWatchListUITestsLaunchTests.swift
+//  KDWatchListUITests
+//
+//  Created by Rob Ranf on 8/30/23.
+//
+
+import XCTest
+
+final class KDWatchListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
